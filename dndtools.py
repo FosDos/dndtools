1358ae0ec934148d56fd18b3e745f681a669ebca
FosDos <fosterclarksonwilliams@gmail.com>
2020-08-16 23:11:35 -0400

    adding stat bonus values

--- dndtools.py
+++ dndtools.py
@@ -7,7 +7,13 @@
 #
 #
 import random;
+import math;
 def dice_roll():
 	return random.randint(1,6);
 def dice_roll(sides):
 	return random.randint(1,sides);
+def stat_to_bonus(stat):
+	return math.floor((stat-10)/2);
+def bonus_test():
+	for x in range(1,31):
+		print(str(x) + ": " + str(stat_to_bonus(x)));
